7335b921c783b3ac4371144d9ee809fdbce6a838
Tanya <tanya.sanjuan@gmail.com>
2025-10-19 22:12:06 +0100

    Printing of bank holidays in Northern Ireland, but excluding BH in the rest of the UK.

--- Week02/assignment02-bankholdiays.py
+++ Week02/assignment02-bankholdiays.py
@@ -1,8 +1,22 @@
 # This program print out the dates of the bank holidays that happen in Northern Ireland
+# excluding the bh celebrated in other parts of UK
 # This program use the next Url with JSON data:
 # https://www.gov.uk/bank-holidays.json
 # Author: Tanya San Juan
 
 # We can use requests module instead to import Jason
 # to request to a website the data in study.
+# source: https://www.w3schools.com/python/module_requests.asp
 
+import requests
+url = 'https://www.gov.uk/bank-holidays.json'
+response = requests.get(url)
+data = response.json()
+ni_bankholidays = data['northern-ireland']['events']
+
+for bh in ni_bankholidays:
+    if bh not in data ['england-and-wales']['events'] and bh not in data['scotland']['events']:
+        print (f'{bh['title']} is on {bh['date']}')
+
+# This prints two festivities: St. Patrick's day and Orangemen's day. 
+# From 2024 until 2027
