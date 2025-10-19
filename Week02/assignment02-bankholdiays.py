70d33612e37442ad70a003fdba8347c7959683d9
Tanya <tanya.sanjuan@gmail.com>
2025-10-19 21:41:19 +0100

    Creation of the doc and definition of the assingment.

---
+++ Week02/assignment02-bankholdiays.py
@@ -0,0 +1,8 @@
+# This program print out the dates of the bank holidays that happen in Northern Ireland
+# This program use the next Url with JSON data:
+# https://www.gov.uk/bank-holidays.json
+# Author: Tanya San Juan
+
+# We can use requests module instead to import Jason
+# to request to a website the data in study.
+
